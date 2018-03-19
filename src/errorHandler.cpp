c5ffd72eb0be4ad32cdd15d5cf18138de363f1cf
Eldar Hauge Torkelsen <eldarht@stud.ntnu.no>
2018-03-19 12:03:38 +0100

    Added error callback for glfw

---
+++ src/errorHandler.cpp
@@ -0,0 +1,7 @@
+#include "header/errorHandling.hpp";
+
+void glfw_errorCallback(int error, const char* description){
+
+	printf("Error:\tfrom glfw with errorcode %d and description \"%s\n\"", error, description);
+	
+}
