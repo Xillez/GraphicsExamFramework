f44d367cc3f353680a7110584f8e90ebdf89441d
kentwh <kentwh@stud.ntnu.no>
2018-03-28 17:41:14 +0200

    Added doxygen comments to functions within 'glfw_eventHandler.hpp'

--- header/glfw_eventHandler.hpp
+++ header/glfw_eventHandler.hpp
@@ -5,10 +5,59 @@
 class EventHandler
 {
     public:
+		/**
+		 * @brief Function to handle keyboard events sent to window.
+		 *
+		 * @param
+		 * @param
+		 * @param
+		 * @param
+		 * @param
+		 */
         void onKeyboardInput();
+
+        /**
+         * @brief Function to handle mouse events to window.
+         *
+         * @param
+         * @param
+         * @param
+         * @param
+         * @param
+         */
         void onMouseInput();
+
+        /**
+		 * @brief Function to handle resume events (when game is paused and user presses ESC).
+		 *
+		 * @param
+		 * @param
+		 * @param
+		 * @param
+		 * @param
+		 */
         void onResume();
+
+        /**
+		 * @brief Function to handle pause events (when game is unpaused and user presses ESC).
+		 *
+		 * @param
+		 * @param
+		 * @param
+		 * @param
+		 * @param
+		 */
         void onPause();
+
+        /**
+		 * @brief Function to handle tick events (happens every frame).
+		 *
+		 * @param
+		 * @param
+		 * @param
+		 * @param
+		 * @param
+		 */
         void onTick();
     protected:
         //
