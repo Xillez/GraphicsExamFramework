f2a04c5aec0530b98c075bff77e64b31f8f682cd
kentwh <kentwh@stud.ntnu.no>
2018-04-11 16:46:29 +0200

    Added all event functions to 'global_function.hpp' and 'global_function.cpp'

--- header/global_function.hpp
+++ header/global_function.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "../header/globalVar.hpp"
 #include <string>
 #include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 /**
  * @brief Process the file data and fetch the moves set for every piece.
@@ -9,3 +12,73 @@
  */
 void getMoves();
 
+/**
+ * @brief Function for setup of eventhandling callback functions.
+ */
+void setup_EventHandling();
+
+/**
+ * @brief Function to handle keyboard events sent to window.
+ *
+ * @param
+ * @param
+ * @param
+ * @param
+ * @param
+ */
+// void onKeyboardInput();
+
+/**
+ * @brief Function to handle mouse events to window.
+ *
+ * @param window - GLFWwindow* - Window in context.
+ * @param xpos - double - x position at time of event.
+ * @param ypos - double - y position at time of event.
+ */
+void OnMouseMove(GLFWwindow *window, double xpos, double ypos);
+
+/**
+ * @brief Function to handle mouse events to window.
+ *
+ * @param
+ * @param
+ * @param
+ * @param
+ * @param
+ */
+// void onMouseClick(GLFWwindow* window, int button, int action, int mods);
+
+/**
+ * @brief Function to handle resume events (when game is paused and user
+ * presses ESC).
+ *
+ * @param
+ * @param
+ * @param
+ * @param
+ * @param
+ */
+// void onResume();
+
+/**
+ * @brief Function to handle pause events (when game is unpaused and
+ * user presses ESC).
+ *
+ * @param
+ * @param
+ * @param
+ * @param
+ * @param
+ */
+// void onPause();
+
+/**
+ * @brief Function to handle tick events (happens every frame).
+ *
+ * @param
+ * @param
+ * @param
+ * @param
+ * @param
+ */
+// void onTick();
--- src/global_function.cpp
+++ src/global_function.cpp
@@ -1,9 +1,9 @@
 #include "../header/global_function.hpp"
-#include "../header/yaml_parser.hpp"
-#include "../header/globalVar.hpp" 		
+#include "../header/yaml_parser.hpp" 		
 #include <iostream>
-std::unordered_map<std::string, std::vector<std::string>> moves; 
+#include <stdio.h>
 
+std::unordered_map<std::string, std::vector<std::string>> moves; 
 
 void getMoves()
 {
@@ -33,4 +33,34 @@ void getMoves()
 			std::cout << i.first << " " << v << '\n';
 		}
 	}
-};
+}
+
+void setup_EventHandling()
+{
+	extern GLFWwindow* window;
+	glfwSetCursorPosCallback(window, ::OnMouseMove);
+	//glfwSetMouseButtonCallback(this->window, [](GLFWwindow* window, int button, int action, int mods) { std::cout << button << ":" << action << ":" << mods << "\n"; });
+}
+
+void OnMouseMove(GLFWwindow *window, double xpos, double ypos)
+{
+	std::cout << xpos << ":" << ypos << "\n";
+    GLint viewport[4];
+    GLfloat winY, z;
+    glGetIntegerv(GL_VIEWPORT, viewport);
+
+    winY = viewport[3] - ypos;
+
+    glReadPixels(xpos, winY, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &z);
+    glm::vec3 screen = glm::vec3(xpos, winY, z);
+
+    glm::mat4 viewMatrix = glm::lookAt(
+		glm::vec3(0, 0, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+    glm::mat4 ProjectionMatrix = glm::perspective(
+        45.0f, (GLfloat)viewport[2] / (GLfloat)viewport[3], 0.1f, 500.0f);
+	glm::vec3 pos3D;
+    pos3D = glm::unProject(screen, viewMatrix, ProjectionMatrix,
+		glm::vec4(viewport[0], viewport[1], viewport[2], viewport[3]));
+
+	printf("%f : %f : %f\n", pos3D.x, pos3D.y, pos3D.z);
+}
