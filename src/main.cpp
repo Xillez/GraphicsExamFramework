3e8f4d91daacedb98cf7e1a267c70d8f560d50f1
Eldar Hauge Torkelsen <eldarht@stud.ntnu.no>
2018-04-16 20:23:04 +0200

    Added info logging for main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "../header/gl_setup.hpp"
 #include "../header/board.hpp"
 #include "../header/camera.hpp"
-//#include "../header/globalVar.hpp"
+#include "../header/globalVar.hpp"
 #include "../class/ShaderManager.hpp"
 
 #include <GLFW/glfw3.h>
@@ -17,41 +17,54 @@ ShaderManager* shaderManager;
 int main(int argc, char const *argv[])
 {	
 	// Ready moves for pieces
+	printf("%s: Getting moves\n",TAG_INFO.c_str());
 	getMoves();
 
 	// Create camera
+	printf("%s: Setting up camera\n",TAG_INFO.c_str());
 	//camera = new Camera(glm::vec3(1, 1, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 	//camera = new Camera(glm::vec3(0, 5, 0), glm::vec3(0, 0, 0), glm::vec3(1, 0, 0));
-	camera = new Camera(glm::vec3(3, 4, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	camera = new Camera(glm::vec3(0, 4, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 	//camera = new Camera(glm::vec3(0, 0, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 	//camera = new Camera(glm::vec3(0, 2, 5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 
 	// Create common interface for shaders
+	printf("%s Creating shadermanager\n", TAG_INFO.c_str());
 	shaderManager = new ShaderManager();
 
 	// OpenGL setup
+	printf("%s Creating window\n", TAG_INFO.c_str());
 	window = glfw_setup();
 	
 	// Make chessboard
+	printf("%s Creating board\n", TAG_INFO.c_str());
 	chessBoard = new Board ("../asset/modell_chessBoard.obj");
 
+	// setup event handler
+	printf("%s Setting up event handler\n", TAG_INFO.c_str());
 	setup_EventHandling();
 
 	bool eh = false;
 	float time = 0.0f;
+	float lastTime = glfwGetTime(); 
 	// Run until close event is given to the window
+	printf("%s Starting gameloop\n", TAG_INFO.c_str());
 	while(!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.4, 0.8, 0.8, 1);
 		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		float currentTime = glfwGetTime();
+		float dt = currentTime - lastTime;
+		lastTime = currentTime;
 
+		camera->rotateBy(dt, glm::vec3(0.0f, 1.0f, 0.0f));
         chessBoard->draw();
 
 		if(eh){
-			chessBoard->movePiece(1, 1, 1, 2);
-			chessBoard->movePiece(1, 6, 1, 5);
-			chessBoard->movePiece(0, 0, 0, 4);
-			chessBoard->movePiece(2, 0, 0, 2);
+			//chessBoard->movePiece(1, 1, 1, 2);
+			//chessBoard->movePiece(1, 6, 1, 5);
+			//chessBoard->movePiece(0, 0, 0, 4);
+			//chessBoard->movePiece(2, 0, 0, 2);
 			eh = false;
 		}else {
 			time += 0.1f;
@@ -65,6 +78,7 @@ int main(int argc, char const *argv[])
 
 	}
 
+	printf("%s Closing application\n", TAG_INFO.c_str());
 	glfwDestroyWindow(window);
 
 	return 0;
