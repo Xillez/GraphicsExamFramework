5731f9e580f18f729f3e8508974a6fade19b5ab4
Eldar Hauge Torkelsen <eldarht@stud.ntnu.no>
2018-04-16 20:19:35 +0200

    added Tags and color for output

--- header/globalVar.hpp
+++ header/globalVar.hpp
@@ -5,12 +5,21 @@
 #include <unordered_map>
 #include "../class/model.hpp"
 
+#define COLOR_RED     "\x1b[31m"
+#define COLOR_GREEN   "\x1b[32m"
+#define COLOR_YELLOW  "\x1b[33m"
+#define COLOR_BLUE    "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
+#define COLOR_CYAN    "\x1b[36m"
+#define COLOR_RESET   "\x1b[0m"
 
+const std::string TAG_DEBUG = COLOR_CYAN "[DEBUG]:\t" COLOR_RESET;
+const std::string TAG_INFO 	= COLOR_GREEN "[INFO]:\t" COLOR_RESET;
+const std::string TAG_ERROR = COLOR_RED "[ERROR]:\t" COLOR_RESET;
 
 const glm::vec2 relativeWindowSize = glm::vec2(60, 60);     //!< percentage size of width and hight relative to monitor. 
 
 const float PI = 3.14f;
-
 
 //extern Shader* shaderProgram;
 
