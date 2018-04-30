11b07751f73454f512d47601a726a7cf26fb7398
Eldar Hauge Torkelsen <eldarht@stud.ntnu.no>
2018-04-30 12:02:32 +0200

    Changed return variables for moveParser functions and added functions for pseudocode

--- helpers/moveParser.cpp
+++ helpers/moveParser.cpp
@@ -3,9 +3,20 @@
 #include <stdio.h>
 #include <utility>
 
-std::vector<std::pair<int, int>> helpers::parseMoveList(std::vector<std::string> moveList){
+
+std::vector<								// All resulting moves
+	std::pair<
+		std::pair<int, int>,				// Destination of parsed move.
+		std::vector<std::function<bool()>>	// Validators to check if move is legal.
+	>
+> helpers::parseMoveList(std::vector<std::string> moveList){
 	
-	std::vector<std::pair<int,int>> alteredMoveList;
+	std::vector<								// All resulting moves
+		std::pair<
+			std::pair<int, int>,				// Destination of parsed move.
+			std::vector<std::function<bool()>>	// Validators to check if move is legal.
+		>
+	> alteredMoveList;				// Variable to return
 
 
 	for(std::string it : moveList){
@@ -15,9 +26,15 @@ std::vector<std::pair<int, int>> helpers::parseMoveList(std::vector<std::string>
 	return alteredMoveList;
 }
 
-std::pair<int, int> helpers::parseMove(std::string move)
+std::pair<
+	std::pair<int, int>,				// Destination of parsed move.
+	std::vector<std::function<bool()>>	// Validators to check if move is legal.
+> helpers::parseMove(std::string move)
 {
-	std::pair<int, int> availableMoves;
+	std::pair<
+		std::pair<int, int>,				// Destination of parsed move.
+		std::vector<std::function<bool()>>	// Validators to check if move is legal.
+	> availableMoves;				// Variable to return
 
 	while(move.length() != 0)
 	{
@@ -26,9 +43,14 @@ std::pair<int, int> helpers::parseMove(std::string move)
 		switch(current){
 			case '(':		//Start parsing group
 				{
-					std::pair<int, int> innerQuery = parseMove(move);	// Get result of inner move.
-					availableMoves.first += innerQuery.first;			// add result to currenty parsing		
-					availableMoves.second += innerQuery.second;			// add result to currenty parsing
+					auto innerQuery = parseMove(move);	// Get result of inner move.
+
+					availableMoves.first.first += innerQuery.first.first;		// add move result to currenty parsing.		
+					availableMoves.first.second += innerQuery.first.second;		// add move result to currenty parsing.
+
+					availableMoves.second.insert(								// Appending validators to current parsing.
+						std::end(availableMoves.second), std::begin(innerQuery.second), std::end(innerQuery.second)
+					);
 					break;
 				}
 			case ')':		//End parsing group
@@ -36,23 +58,23 @@ std::pair<int, int> helpers::parseMove(std::string move)
 				break;
 
 			case 'F':		//Move forward
-				availableMoves.second++;
+				availableMoves.first.second++;
 				break;
 
 			case 'R':		//Move right
-				availableMoves.first++;
+				availableMoves.first.first++;
 				break;
 
 			case 'B':		//Move back
-				availableMoves.second--;
+				availableMoves.first.second--;
 				break;
 
 			case 'L':		//Move left
-				availableMoves.first--;
+				availableMoves.first.first--;
 				break;
 
 			case '$':		//Start action
-				//availableMoves.first++;
+				parseAction(move);
 				break;
 
 			default:
@@ -63,3 +85,41 @@ std::pair<int, int> helpers::parseMove(std::string move)
 		}
 	}
 }
+
+std::pair<
+	std::pair<int, int>,				// Destination of parsed move.
+	std::vector<std::function<bool()>>	// Validators to check if move is legal.
+> helpers::parseAction(std::string move){
+
+	char current = move.at(0);		// get the next character.
+	move.erase(0,1);				// remove character from list.
+	switch(current){	
+		case 'X':
+			return parseAttack(move);
+			break;
+		case 'M':
+			return parseMove(move);
+			break;
+		case 'R':
+			return parseReplace(move);
+			break;
+		default:
+			printf("%s unexpected value: %c\n", TAG_WARN.c_str(), current);
+			exit(-1);
+			break;
+	}
+}
+
+std::pair<
+	std::pair<int, int>,				// Destination of parsed move.
+	std::vector<std::function<bool()>>	// Validators to check if move is legal.
+> helpers::parseAttack(std::string move){
+
+};
+
+std::pair<
+	std::pair<int, int>,				// Destination of parsed move.
+	std::vector<std::function<bool()>>	// Validators to check if move is legal.
+> helpers::parseReplace(std::string move){
+
+};
